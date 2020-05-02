3ff783367d26bb775b5398bac5b4aaffe81046de
s3806867 <s3806867@rmit.edu.vn>
2020-05-02 14:12:28 +0700

    tesmtes

--- Week 8/index.php
+++ Week 8/index.php
@@ -6,6 +6,11 @@
     <title>Hello world</title>
 </head>
 <body>
+    <!--Assume that movies are only every 3 hour from 12pm to 12am
+    Valid time should be T12, T15, T18, T21, T00.
+    Mon-Fri all time discount except for T18 and T21
+    Sat & Sun dis for only T00 test
+    -->
     <?php
         echo "<h1>Hello World! </h1>";
         echo "<p>My name is $name.</p>";
